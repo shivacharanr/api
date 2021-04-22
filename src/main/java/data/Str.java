118b54d4a0605b74755f5d6a123ea08480769027
shivacharanr <prinzesiva@gmail.com>
2021-04-22 22:07:01 +0530

    change to str class file

--- src/main/java/data/Str.java
+++ src/main/java/data/Str.java
@@ -8,6 +8,7 @@ public class Str {
 		String str= "{status:OK,place_id:7729f0af09c9ad9bfa44965685c3d14c,scope:APP,reference:380eb48d6db863b08cdf0c2a180e985b380eb48d6db863b08cdf0c2a180e985b,id:380eb48d6db863b08cdf0c2a180e985b}";
 		
 		System.out.println(str.substring(str.lastIndexOf("place_id:"), str.indexOf(",", str.lastIndexOf("place_id:"))));
+		System.out.println("shiva");
 
 	}
 
